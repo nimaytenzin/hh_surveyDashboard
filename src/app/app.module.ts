43664face820520685befb785d9868ddc49fe853
nimaytenzin <nimaytenzin@gmail.com>
2021-03-10 19:43:10 +0600

    dsds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { ErrorComponent } from './error/error.component';
-import { SelectZoneComponent } from './select-zone/select-zone.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RegisterAtmComponent } from './register-atm/register-atm.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 import { MatTableModule } from '@angular/material/table'
@@ -31,42 +28,16 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpInterceptorService } from './service/http-interceptor.service';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatListModule } from '@angular/material/list';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { MapComponent } from './map/map.component';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { RegisterUnitComponent } from './register-unit/register-unit.component';
-import { CameraComponent } from './camera/camera.component';
 import {WebcamModule} from 'ngx-webcam';
-import { UploadImageComponent } from './upload-image/upload-image.component';
 import { AdminComponent } from './admin/admin.component';
-import { DataTableComponent} from './DataTable/data-table/data-table.component'
-import { DialogBoxComponent } from './DataTable/dialog-box/dialog-box.component';
-import { EditUnitComponent } from './edit-unit/edit-unit.component';
-import { EditBuildingComponent } from './edit-building/edit-building.component';
-import { MemberTableComponent } from './member-table/member-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    RegisterAtmComponent,
     ErrorComponent,
-    SelectZoneComponent,
     DashboardComponent,
-    ChangePasswordComponent,
-    MapComponent,
-    ConfirmDialogComponent,
-    RegisterUnitComponent,
-    CameraComponent,
-    UploadImageComponent,
-    RegisterAtmComponent,
     AdminComponent,
-    DataTableComponent,
-    DialogBoxComponent,
-    EditUnitComponent,
-    EditBuildingComponent,
-    MemberTableComponent
   ],
   imports: [
     BrowserModule,
@@ -99,6 +70,6 @@ import { MemberTableComponent } from './member-table/member-table.component';
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  entryComponents: [ ConfirmDialogComponent, DialogBoxComponent]
+  entryComponents: []
 })
 export class AppModule { }
